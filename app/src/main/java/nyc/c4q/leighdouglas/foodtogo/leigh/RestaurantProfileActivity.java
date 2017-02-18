8884cc13d4c69456aa0502c7036cc15f228729ae
HakeemSackes-Bramble <hakksaxx@outlook.com>
2017-02-18 14:26:05 -0500

    making notification

--- app/src/main/java/nyc/c4q/leighdouglas/foodtogo/leigh/RestaurantProfileActivity.java
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/leigh/RestaurantProfileActivity.java
@@ -1,8 +1,12 @@
 package nyc.c4q.leighdouglas.foodtogo.leigh;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +19,7 @@ import nyc.c4q.leighdouglas.foodtogo.R;
 
 public class RestaurantProfileActivity extends AppCompatActivity {
 
+    private static final int NOTIFICATION_ID = 555;
     private EditText businessName;
     private EditText addressLine1;
     private EditText addressLine2;
@@ -23,6 +28,10 @@ public class RestaurantProfileActivity extends AppCompatActivity {
     private EditText pickupTime;
     private EditText additionInstructions;
     private Button notifyButton;
+    private NotificationCompat.Builder nCBuilder;
+    long requestID;
+    private PendingIntent pIntent;
+
 
 
     @Override
@@ -34,7 +43,7 @@ public class RestaurantProfileActivity extends AppCompatActivity {
 
     }
 
-    private void initViews(){
+    private void initViews() {
         businessName = (EditText) findViewById(R.id.business_name);
         addressLine1 = (EditText) findViewById(R.id.address_line1);
         addressLine2 = (EditText) findViewById(R.id.address_line2);
@@ -43,10 +52,18 @@ public class RestaurantProfileActivity extends AppCompatActivity {
         pickupTime = (EditText) findViewById(R.id.pick_up_time);
         additionInstructions = (EditText) findViewById(R.id.additional_instructions);
         notifyButton = (Button) findViewById(R.id.notify_button);
+        requestID = System.currentTimeMillis();
+
+        nCBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext()).setContentIntent(pIntent);
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                notificationManager.notify(NOTIFICATION_ID, nCBuilder.build());
+                /** TODO:
+                 *
+                 */
                 //save data to sqlite database
                 //make phone notification
             }
