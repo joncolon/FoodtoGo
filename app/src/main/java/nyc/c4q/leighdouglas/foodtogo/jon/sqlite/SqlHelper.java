7280f62eb5137b49b4ab36231abe821f95872b88
Jonathan Colon <jonatron2.0@gmail.com>
2017-02-18 14:00:33 -0500

    restaurant db added

---
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/jon/sqlite/SqlHelper.java
@@ -0,0 +1,36 @@
+package nyc.c4q.leighdouglas.foodtogo.jon.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.qbusict.cupboard.QueryResultIterable;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+
+public class SqlHelper {
+
+    public SqlHelper() {
+    }
+
+    public static List<Restaurant> selectAllRestaurants(SQLiteDatabase db) {
+        List<Restaurant> restaurants = new ArrayList<>();
+        try {
+            QueryResultIterable<Restaurant> itr = cupboard().withDatabase(db).query(Restaurant.class).query();
+            for (Restaurant restaurant : itr) {
+                restaurants.add(restaurant);
+            }
+            itr.close();
+        } catch (Exception e) {
+            Log.e("Restaurant List", "selectAllRestaurants: ", e);
+        }
+        return restaurants;
+    }
+
+    public Restaurant getRestaurantByName(SQLiteDatabase db, String restaurantName) {
+        return cupboard().withDatabase(db).query(Restaurant.class).withSelection("restaurantName = ?", restaurantName).get();
+    }
+}
