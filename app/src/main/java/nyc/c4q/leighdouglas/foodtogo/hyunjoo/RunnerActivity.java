85dc96a622d6eb47ef96032b6c4e73c82974f9f8
Jonathan Colon <jonatron2.0@gmail.com>
2017-02-18 23:31:46 -0500

    click listeners

--- app/src/main/java/nyc/c4q/leighdouglas/foodtogo/hyunjoo/RunnerActivity.java
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/hyunjoo/RunnerActivity.java
@@ -13,6 +13,5 @@ public class RunnerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
--- app/src/main/java/nyc/c4q/leighdouglas/foodtogo/leigh/RestaurantViewHolder.java
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/leigh/RestaurantViewHolder.java
@@ -2,6 +2,7 @@ package nyc.c4q.leighdouglas.foodtogo.leigh;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import nyc.c4q.leighdouglas.foodtogo.R;
@@ -13,7 +14,8 @@ import nyc.c4q.leighdouglas.foodtogo.jon.models.Restaurant;
 
 public class RestaurantViewHolder extends RecyclerView.ViewHolder {
     private TextView nameTV, address1TV, address2TV, phoneTV, pickupTV, instructionTV;
-
+    private ImageButton claimIB, mapIB;
+    private RestaurantAdapter.Listener listener;
     public RestaurantViewHolder(View itemView) {
         super(itemView);
         nameTV = (TextView)itemView.findViewById(R.id.restaurant_name);
@@ -23,12 +25,19 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder {
         pickupTV = (TextView)itemView.findViewById(R.id.availability);
     }
 
-    public void bind(Restaurant restaurant){
+    public void bind(final Restaurant restaurant){
         nameTV.setText(restaurant.getBusinessName());
         address1TV.setText(restaurant.getAddressLine1());
         address2TV.setText(restaurant.getAddressLine2());
         phoneTV.setText(restaurant.getPhoneNumber());
         pickupTV.setText(restaurant.getPickupTime());
+
+        claimIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onRestaurantClicked(restaurant);
+            }
+        });
 
     }
 }
