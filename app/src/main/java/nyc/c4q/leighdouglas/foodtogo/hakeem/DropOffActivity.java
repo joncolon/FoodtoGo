3f0ac9b634fc095aded0c0cd145c34cd8fcbc762
HakeemSackes-Bramble <hakksaxx@outlook.com>
2017-02-18 23:40:51 -0500

    commiting to pull

---
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/hakeem/DropOffActivity.java
@@ -0,0 +1,8 @@
+package nyc.c4q.leighdouglas.foodtogo.hakeem;
+
+/**
+ * Created by hakeemsackes-bramble on 2/18/17.
+ */
+
+public class DropOffActivity {
+}
---
+++ app/src/main/java/nyc/c4q/leighdouglas/foodtogo/hakeem/SignUpActivityFirebase.java
@@ -0,0 +1,62 @@
+package nyc.c4q.leighdouglas.foodtogo.hakeem;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import nyc.c4q.leighdouglas.foodtogo.R;
+
+/**
+ * Created by leighdouglas on 2/18/17.
+ */
+
+public class SignUpActivityFirebase extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static String TAG = "FireBase";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }// ...
+            }
+        };
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
